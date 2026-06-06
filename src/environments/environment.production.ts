19f490e3cb9f224edbdf5f525b89b268a8a2b815
DangDuc1523 <dangduc504@gmail.com>
2026-06-06 16:57:50 +0700

    edit env final

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Thay bằng URL Railway của bạn sau khi deploy
-  apiUrl: 'https://YOUR-APP.up.railway.app/api'
+  apiUrl: 'https://trasua-backend.onrender.com/api'
 };
